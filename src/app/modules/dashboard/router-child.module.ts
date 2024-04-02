a54be35a63b2fa0e31695dd666b89fe6a62f600d
Jorge Rivera <jorge.rivera@ssde.com.mx>
2024-04-01 20:13:44 -0600

    Angular-Material. Added category module. Added category component. Added component path to dashboard child routes

--- src/app/modules/dashboard/router-child.module.ts
+++ src/app/modules/dashboard/router-child.module.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { CategoryComponent } from '../category/components/category/category.component';
 
 const dashboardChildRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'category', component: CategoryComponent }
 ]
 
 @NgModule({
