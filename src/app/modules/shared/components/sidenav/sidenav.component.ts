b6cb321c7785313e4ea48f8cc894401da261624e
Jorge Rivera <jorge.rivera@ssde.com.mx>
2024-04-01 19:22:01 -0600

    Angular. Added navbar component, Added sidenav component. Added code to shared module code to show recently added components

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit, OnDestroy{
+  mobileQuery: MediaQueryList;
+  menuNav = [
+    { name: "Home", route: "home", icon: "home" },
+    { name: "Categorias", route: "category", icon: "category" },
+    { name: "Productos", route: "product", icon: "production_quantity_limits"}
+  ];
+
+  private _mobileQueryListener: () => void;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery = media.matchMedia('(max-width: 650px)');
+    this.mobileQuery.addEventListener("change", this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener("change", this._mobileQueryListener);
+  }
 
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { MaterialModule } from './material.module';
+import { RouterModule } from '@angular/router';
+// import { FlexLayoutModule } from "@angular/flex-layout";
 
 
 
@@ -9,7 +12,13 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     SidenavComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    // FlexLayoutModule,
+    RouterModule,
+    MaterialModule
+  ],
+  exports: [
+    SidenavComponent
   ]
 })
 export class SharedModule { }
